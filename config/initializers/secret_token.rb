4b1bf61d1ad77c14f217efcb3d1a7f9855662079
Fuka <hikaru.minagoshi@gmail.com>
2017-03-24 17:28:06 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kinari::Application.config.secret_key_base = '688acec1f65338f04d2653758579d5a0bc797b394b39b2a10c0d2aba7ecb51d818fa6ec9ff60a6c552e5feda4d92d1180d03f0647fa4f0526f89f4ef3e139d45'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kinari::Application.config.session_store :cookie_store, key: '_Kinari_session'
