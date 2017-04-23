52119199c6e1caf99bd892700328791103104621
Fuka <hikaru.minagoshi@gmail.com>
2017-04-23 07:07:05 +0000

    Add user microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   has_many :microposts, dependent: :destroy
+  
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+  
   # test "the truth" do
   #   assert true
   # end
